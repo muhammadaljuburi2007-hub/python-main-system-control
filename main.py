a19ba6048bd901c7846b21040355c4177ad07bda
dechtel <muhammad.aljuburi2007@gmail.com>
2026-03-29 13:02:19 +0300

    Fix comment formatting in main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tkinter as tk
 import os 
 gui = tk.Tk()
 gui.geometry("200x200")
-##----labels----buttons---others##
+##----labels----buttons---others---##
 def poweroffs() :
 	os.system("poweroff")
 def suspends():
@@ -13,6 +13,8 @@ def reboots():
 	os.system("reboot")
 def hibs():
 	os.system("sudo systemctl hibernate")
+
+
 poweroff=tk.Button(gui,text='Shutdown',command=poweroffs)
 reboot=tk.Button(gui,text="Restart",command=reboots)
 suspend=tk.Button(gui,text="Sleep",command=suspends)
@@ -21,11 +23,6 @@ poweroff.grid(row=0,column=0,columnspan=2)
 reboot.grid(row=0,column=2,columnspan=2)
 suspend.grid(row=1,column=0,columnspan=2)
 hib.grid(row=1,column=2,columnspan=2)
-
-
-
-
-
 
 
 #---mainloop
